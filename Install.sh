54169a2095d32a96500c199c7177b70fe50024a5
zereft01 <80718011+zereft01@users.noreply.github.com>
2022-03-31 15:03:38 -0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,7 +1,7 @@
 green='\033[1;32m'
 lineas="==================================="
 printf $green
-echo "Estas por instalar las herramientas necesarias para usar DMusic"
+echo "Estas por instalar las herramientas necesarias para usar YouDownload"
 read -p "Selecciona tu sistema:
 [1]Termux 
 [2]Ubuntu 
@@ -15,15 +15,15 @@ case $os in
 		apt install ffmpeg
 		apt install python
 		pip install youtube-dl
-		chmod +x DMusic
-		echo "Instalaciòn finalizada. Para iniciar a descargar musica ejecuta ./DMusic"
+		chmod +x YouDownload
+		echo "Instalación finalizada. Para iniciar la herramienta ejecuta ./YouDonload"
 	;;
 	[Uu]buntu|2) 
 		echo $lineas
 		sudo apt install ffmpeg
 		sudo apt install youtube-dl
-		chmod +x DMusic
-		echo "Instalaciòn finalizada. Para iniciar a descargar musica ejecuta ./DMusic"
+		chmod +x YouDownload
+		echo "Instalaciòn finalizada. Para iniciar a descargar musica ejecuta ./YouDownload"
 	;;
 	[3]|*)
 		break
