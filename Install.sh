dad6aa632c7ab616dcf55192cbde15cd876487dd
zereft01 <80718011+zereft01@users.noreply.github.com>
2022-03-31 15:27:51 -0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,15 +1,33 @@
-green='\033[1;32m'
+clear
 lineas="==================================="
-printf $green
+red='\033[1;31m'                                        
+green='\033[1;32m'
+yellow='\033[1;33m'                                   
+blue='\033[1;34m'                                       
+magenta='\033[1;35m'                                    
+cyan='\033[1;36m'
+reset='\033[0m'
+printf $red
+
+echo "
+╻┏┓╻┏━┓╺┳╸┏━┓╻  ╻   ┏━┓╻ ╻
+┃┃┗┫┗━┓ ┃ ┣━┫┃  ┃   ┗━┓┣━┫
+╹╹ ╹┗━┛ ╹ ╹ ╹┗━╸┗━╸╹┗━┛╹ ╹
+
+"
+printf $reset
+printf $red
 echo "Estas por instalar las herramientas necesarias para usar YouDownload"
 read -p "Selecciona tu sistema:
 [1]Termux 
 [2]Ubuntu 
 [3]Salir " os 
+printf $reset
 
 
 case $os in
 	[Tt]ermux|1)
+		printf $green
 		echo $lineas
 		apt update -y
 		apt install ffmpeg
@@ -17,14 +35,20 @@ case $os in
 		pip install youtube-dl
 		chmod +x YouDownload
 		echo "Instalación finalizada. Para iniciar la herramienta ejecuta ./YouDonload"
+		printf $reset
 	;;
 	[Uu]buntu|2) 
+		printf $cyan
 		echo $lineas
 		sudo apt install ffmpeg
 		sudo apt install youtube-dl
 		chmod +x YouDownload
-		echo "Instalaciòn finalizada. Para iniciar a descargar musica ejecuta ./YouDownload"
+		echo "Instalaciòn finalizada. Para iniciar la herramienta ejecuta ./YouDownload"
+		printf $reset
 	;;
 	[3]|*)
-		break
+		printf $blue
+		echo $lineas
+		echo "              Bye"
+		echo $lineas
 esac
